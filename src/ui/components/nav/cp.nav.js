d3c9b2af6054ad411b30c827b7eea2924301f9e2
Mahdi Motamedi <mahdi.moatamedi@yahoo.com>
2019-08-21 00:34:01 +0430

    feat: add nav

---
+++ src/ui/components/nav/cp.nav.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import { Container, FormControl, InputGroup } from "react-bootstrap";
+import logoDark from "../../../assets/images/logo.svg";
+import mapIcon from "../../../assets/images/map.svg";
+
+function CpNav() {
+  const [onTop, setOnTop] = useState(true);
+  const [haveScrolled, setHaveScrolled] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, true);
+
+    return function cleanup() {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  const handleScroll = () => {
+    setOnTop(window.scrollY === 0);
+    setHaveScrolled(true);
+  };
+
+  return (
+    <nav
+      className={`nav ${
+        onTop ? (haveScrolled ? "nav-no-sticky" : "") : "nav-sticky"
+      }`}
+    >
+      <Container>
+        <div className="nav__logo-part">
+          <img src={logoDark} alt="Uber Eats" />
+        </div>
+
+        <div className="nav__deliver-box">
+          <div className="nav__deliver-box--container">
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text>
+                  <img src={mapIcon} alt="map-icon" />
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl placeholder="Enter delivery address" />
+            </InputGroup>
+          </div>
+        </div>
+
+        <div className="nav__sign-in-part">
+          <a href="/">Sign In</a>
+        </div>
+      </Container>
+    </nav>
+  );
+}
+
+export default CpNav;
