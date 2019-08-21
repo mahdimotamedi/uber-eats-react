d977470672bd3216b92bcdcdc1c7a7b1fa95cc8d
Mahdi Motamedi <mahdi.moatamedi@yahoo.com>
2019-08-21 10:33:55 +0430

    feat: modify AppContent

--- src/ui/views/AppContent.js
+++ src/ui/views/AppContent.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import CarouselFood from "../components/carousel-food/CarouselFood";
+import { Container } from "react-bootstrap";
+import ApiFood from "../../core/api/api.food";
+import HomeIntro from "../components/home-intro/HomeIntro";
 
 function AppContent() {
-  return <div>Home</div>;
+  const [foods1, setFoods1] = useState([]);
+  const [foods2, setFoods2] = useState([]);
+
+  useEffect(() => {
+    setFoods1(ApiFood.getFoods({ cuisineType: "American" }));
+    setFoods2(ApiFood.getFoods({ mealType: "Breakfast" }));
+  }, []);
+
+  return (
+    <>
+      <Container>
+        <CarouselFood title="Food Delivery in Piscataway" items={foods1} />
+
+        <hr />
+
+        <HomeIntro />
+
+        <hr />
+
+        <CarouselFood title="New on Uber Eats" items={foods2} />
+
+        <hr />
+      </Container>
+    </>
+  );
 }
 
 export default AppContent;
